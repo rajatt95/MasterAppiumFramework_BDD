57bc52b67b4cf1f8a7659e5eb04e5cdc0aa6e6d4
“rajatt95” <rajatvermaa95@gmail.com>
2022-04-29 02:09:38 +0530

    Details added in files

--- src/main/java/com/appium/utils/UsernameUtils.java
+++ src/main/java/com/appium/utils/UsernameUtils.java
@@ -1,3 +1,15 @@
+/**
+ * @author Rajat Verma
+ * https://www.linkedin.com/in/rajat-v-3b0685128/
+ * https://github.com/rajatt95
+ * https://rajatt95.github.io/
+ *
+ * Course: Appium Mobile Automation - Android & iOS + Frameworks + CICD (https://www.udemy.com/course/the-complete-appium-course-for-ios-and-android/)
+ * Tutor: Omprakash Chavan (https://www.udemy.com/user/omprakash-chavan/)
+ */
+
+/***************************************************/
+
 package com.appium.utils;
 
 public class UsernameUtils {
