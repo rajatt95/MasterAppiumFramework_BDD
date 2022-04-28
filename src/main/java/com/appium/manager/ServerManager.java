f228b35cce05e80ab8f93959edc964e31ef9065b
“rajatt95” <rajatvermaa95@gmail.com>
2022-04-29 02:02:56 +0530

    Updations

--- src/main/java/com/appium/manager/ServerManager.java
+++ src/main/java/com/appium/manager/ServerManager.java
@@ -13,46 +13,46 @@ import com.appium.utils.UsernameUtils;
 
 /**
  * We are creating different server instances for all devices
- * 
+ * <p>
  * We are not using the Server URL from config.properties file
  */
 public class ServerManager {
 
-	private static ThreadLocal<AppiumDriverLocalService> server = new ThreadLocal<>();
+    private static ThreadLocal<AppiumDriverLocalService> server = new ThreadLocal<>();
 
-	TestUtils utils = new TestUtils();
+    TestUtils utils = new TestUtils();
 
-	public AppiumDriverLocalService getServer() {
-		return server.get();
-	}
+    public AppiumDriverLocalService getServer() {
+        return server.get();
+    }
 
-	public void startServer() {
-		utils.log().info("starting appium server");
+    public void startServer() {
+        utils.log().info("starting appium server");
 
-		/* Windows */
-		// AppiumDriverLocalService server = getAppiumServiceForWindows();
+        /* Windows */
+        // AppiumDriverLocalService server = getAppiumServiceForWindows();
 
-		/* MAC */
-		AppiumDriverLocalService server = getAppiumServiceForMac();
-		server.start();
+        /* MAC */
+        AppiumDriverLocalService server = getAppiumServiceForMac();
+        server.start();
 
-		if (server == null || !server.isRunning()) {
-			utils.log().fatal("Appium server not started. ABORT!!!");
-			throw new AppiumServerHasNotBeenStartedLocallyException("Appium server not started. ABORT!!!");
-		}
-		/* This will not print the Appium server Logs in IDE console */
-		server.clearOutPutStreams();
-		this.server.set(server);
-		utils.log().info("Appium server started");
-	}
+        if (server == null || !server.isRunning()) {
+            utils.log().fatal("Appium server not started. ABORT!!!");
+            throw new AppiumServerHasNotBeenStartedLocallyException("Appium server not started. ABORT!!!");
+        }
+        /* This will not print the Appium server Logs in IDE console */
+        server.clearOutPutStreams();
+        this.server.set(server);
+        utils.log().info("Appium server started");
+    }
 
-	public AppiumDriverLocalService getAppiumServerDefault() {
-		return AppiumDriverLocalService.buildDefaultService();
-	}
+    public AppiumDriverLocalService getAppiumServerDefault() {
+        return AppiumDriverLocalService.buildDefaultService();
+    }
 
-	public AppiumDriverLocalService getAppiumServiceForWindows() {
+    public AppiumDriverLocalService getAppiumServiceForWindows() {
 
-		// GlobalParams params = new GlobalParams();
+        // GlobalParams params = new GlobalParams();
 //		return AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
 //				.usingDriverExecutable(new File("C:\\Program Files\\nodejs\\node.exe"))
 //				.withAppiumJS(new File("C:\\Users\\rajatverma01\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js")).usingPort(4723)
@@ -60,26 +60,28 @@ public class ServerManager {
 //				//.withLogFile(new File("ServerLogs/server.log")));
 //				.withLogFile(new File(params.getPlatformName() + "_" + params.getDeviceName() + File.separator + "Server.log")));
 
-		GlobalParams params = new GlobalParams();
-		return AppiumDriverLocalService.buildService(new AppiumServiceBuilder().usingAnyFreePort()
-				.withArgument(GeneralServerFlag.SESSION_OVERRIDE).withLogFile(new File(
-						params.getPlatformName() + "_" + params.getDeviceName() + File.separator + "Server.log")));
-	}
+        GlobalParams params = new GlobalParams();
+        return AppiumDriverLocalService.buildService(new AppiumServiceBuilder().usingAnyFreePort()
+                .withArgument(GeneralServerFlag.SESSION_OVERRIDE).withLogFile(new File(
+                        params.getPlatformName() + "_" + params.getDeviceName() + File.separator + "Server.log")));
+    }
 
-	public AppiumDriverLocalService getAppiumServiceForMac() {
-		GlobalParams params = new GlobalParams();
-		HashMap<String, String> environment = new HashMap<String, String>();
-		environment.put("PATH",
-				"/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin:/Users/\"+UsernameUtils.getUserName()+\"/Library/Android/sdk/tools:/Users/\"+UsernameUtils.getUserName()+\"/Library/Android/sdk/platform-tools:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin"
-						+ System.getenv("PATH"));
-		environment.put("ANDROID_HOME", "/Users/" + UsernameUtils.getUserName() + "/Library/Android/sdk");
-		// environment.put("JAVA_HOME",
-		// "/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home");
-		return AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
-				.usingDriverExecutable(new File("/opt/homebrew/bin/node"))
-				.withAppiumJS(new File("/opt/homebrew/lib/node_modules/appium/build/lib/main.js")).usingAnyFreePort()
-				.withArgument(GeneralServerFlag.SESSION_OVERRIDE).withEnvironment(environment).withLogFile(new File(
-						params.getPlatformName() + "_" + params.getDeviceName() + File.separator + "Server.log")));
+    public AppiumDriverLocalService getAppiumServiceForMac() {
+        GlobalParams params = new GlobalParams();
+        HashMap<String, String> environment = new HashMap<String, String>();
+        environment.put("PATH",
+                "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin:/Users"
+						+ UsernameUtils.getUserName() + "/Library/Android/sdk/tools:/Users/"
+						+ UsernameUtils.getUserName() + "/Library/Android/sdk/platform-tools:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin"
+                        + System.getenv("PATH"));
+        environment.put("ANDROID_HOME", "/Users/" + UsernameUtils.getUserName() + "/Library/Android/sdk");
+        // environment.put("JAVA_HOME",
+        // "/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home");
+        return AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+                .usingDriverExecutable(new File("/opt/homebrew/bin/node"))
+                .withAppiumJS(new File("/opt/homebrew/lib/node_modules/appium/build/lib/main.js")).usingAnyFreePort()
+                .withArgument(GeneralServerFlag.SESSION_OVERRIDE).withEnvironment(environment).withLogFile(new File(
+                        params.getPlatformName() + "_" + params.getDeviceName() + File.separator + "Server.log")));
 
-	}
+    }
 }
